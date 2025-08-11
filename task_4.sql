33075382194a27216a0f9030f4b7a3de98ac4236
elainees-devs <elainees.devs@gmail.com>
2025-08-11 10:08:39 +0300

    add task_4.sql to print full description of books table without DESCRIBE or EXPLAIN

---
+++ task_4.sql
@@ -0,0 +1,17 @@
+USE alx_book_store;
+
+SELECT 
+    COLUMN_NAME,
+    COLUMN_TYPE,
+    IS_NULLABLE,
+    COLUMN_DEFAULT,
+    COLUMN_KEY,
+    EXTRA
+FROM 
+    INFORMATION_SCHEMA.COLUMNS
+WHERE 
+    TABLE_SCHEMA = 'alx_book_store'
+    AND TABLE_NAME = 'Books'
+ORDER BY 
+    ORDINAL_POSITION;
+-- This query retrieves metadata about the columns in the 'Books' table
